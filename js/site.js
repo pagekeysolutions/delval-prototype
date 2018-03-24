0721f506dd0281279ea59172bf0c68b804cbb293
Stephen Grice <stephengrice@live.com>
2018-03-24 19:32:55 -0400

    Add search bar functionality

--- js/site.js
+++ js/site.js
@@ -22,9 +22,28 @@ products = [
   {"name" : "Thrift Shop Children's Home"},
   {"name" : "Grace Baptist Church"},
   {"name" : "South Jersey Cancer Fund"}
-]
+];
 
 $(document).ready(function() {
-  name = products[$_GET["p"] - 1]["name"]
-  $("#product-name").text(name);
+  if ($_GET.length > 0) {
+    name = products[$_GET["p"] - 1]["name"]
+    $("#product-name").text(name);
+  }
+
+  // Search bar functionality
+  $("#search").keyup(function() {
+    search_text = $("#search input").val().toLowerCase();
+    $(".product-box").each(function(index) {
+      box = $(this);
+      console.log("search: " + search_text + " box: " + box.find("h2").text());
+      if (search_text.length < 1 || box.find("h2").text().toLowerCase().indexOf(search_text) >= 0) {
+        // Found. Do not hide
+        box.css("display", "block");
+        console.log("found: " + box.find("h2").val());
+      } else {
+        box.css("display", "none")
+        console.log("hid: " + box.find("h2").val());
+      }
+    });
+  });
 });
